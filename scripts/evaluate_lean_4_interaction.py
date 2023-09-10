b9da10a4d76a8e1bdc7b78ed64b9995ae8840948
josojo <josojo@hotmail.de>
2023-09-10 08:30:15 +0200

    get full proof with full_names

--- scripts/evaluate_lean_4_interaction.py
+++ scripts/evaluate_lean_4_interaction.py
@@ -1,10 +1,12 @@
 
+from pickletools import read_uint1
 import ray
 import random
 import json
 import argparse
 from tqdm import tqdm
 from loguru import logger
+from typing import List
 
 from lean_dojo import *
 from lean_dojo.constants import TACTIC_MEMORY_LIMIT
@@ -33,6 +35,8 @@ def _validate_ground_truth(thm) -> bool:
 
     try:
         with Dojo(theorem, 1000) as (dojo, init_state):
+            if not isinstance(init_state, TacticState):
+                raise TypeError(f"Expected TacticState, got {type(init_state).__name__}")
             assert init_state.num_goals == 1
             init_ctx = [decl.ident for decl in init_state.goals[0].assumptions]
 
@@ -48,6 +52,22 @@ def _validate_ground_truth(thm) -> bool:
 
     return lean_dojo_result
 
+def get_imports(traced_theorem) -> List[str]:
+    traced_file = traced_theorem.traced_file
+    return traced_file.get_direct_dependencies()
+
+def get_proof_with_full_names(traced_theorem) -> str:
+    traced_tactics = traced_theorem.get_traced_tactics()
+    result = ""
+    for tactic in traced_tactics:
+        annotated_tactic, provenances = tactic.get_annotated_tactic()
+        for prov in provenances:
+            full_name = prov['full_name']
+            tactic_name = full_name.split(".")[-1]
+            annotated_tactic = annotated_tactic.replace(f"<a>{tactic_name}</a>", full_name,1)
+        result += annotated_tactic
+
+    return result
 
 @ray.remote
 class RayHelper:
@@ -61,6 +81,7 @@ def main() -> None:
     logger.info(args)
 
     os.environ["TACTIC_TIMEOUT"] = "600000"
+    # repo = LeanGitRepo("https://github.com/josojo/lean4-example", "82b5f8a266b0bc7870228934560d4fc691be752d")
 
     # repo = LeanGitRepo("https://github.com/leanprover-community/mathlib4", "5a919533f110b7d76410134a237ee374f24eaaad")
     repo = LeanGitRepo("https://github.com/yangky11/lean4-example", "7d711f6da4584ecb7d4f057715e1f72ba175c910")
@@ -81,12 +102,16 @@ def main() -> None:
             continue
 
         proof = t.get_single_tactic_proof()
+        list_of_imports = get_imports(t)
+        full_proof = get_proof_with_full_names(t)
+
         print(t)
+        print("imports are", list_of_imports)
         print("proof is ",  proof)
+        print("full proof is", full_proof)
         if proof is None:  # Discard theorems without tactic-style proofs.
             continue
 
-        # namespaces = list(set(inside + opened))
         print(t.theorem.full_name)
         assert t.theorem.full_name not in theorems
         theorems[t.theorem.full_name] = {
